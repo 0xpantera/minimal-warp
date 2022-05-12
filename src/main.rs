2a23b14cd12484c7d572aa20c8222873eb5213a5
Francososa <franky.sosa@gmail.com>
2022-05-12 17:14:23 -0600

    Add custom error handling for route

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ use std::str::FromStr;
 use std::io::{Error, ErrorKind};
 use serde::Serialize;
 
-use warp::Filter;
+use warp::Rejection;
+use warp::hyper::StatusCode;
+use warp::{Filter, reject::Reject, Reply};
 
 #[derive(Debug, Serialize)]
 struct Question {
@@ -37,6 +39,10 @@ impl FromStr for QuestionId {
     }
 }
 
+#[derive(Debug)]
+struct InvalidId;
+impl Reject for InvalidId {}
+
 async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
     let question = Question::new(
         QuestionId::from_str("1").expect("No id provided"),
@@ -45,9 +51,30 @@ async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
         Some(vec!["faq".to_string()])
     );
 
-    Ok(warp::reply::json(
-        &question
-    ))
+    match question.id.0.is_empty() {
+        true => {
+            Err(warp::reject::custom(InvalidId))
+        },
+        false => {
+            Ok(warp::reply::json(
+                &question
+            ))
+        }
+    }
+}
+
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(InvalidId) = r.find() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented",
+            StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+            "Route not found",
+            StatusCode::NOT_FOUND,
+        ))
+    }
 }
 
 #[tokio::main]
@@ -55,7 +82,8 @@ async fn main() {
     let get_items = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
-        .and_then(get_questions);
+        .and_then(get_questions)
+        .recover(return_error);
 
     let routes = get_items;
 
