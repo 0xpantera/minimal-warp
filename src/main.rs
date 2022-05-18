8d279fcb43ce270d089d44afc31b9f3dfaa1fbe7
Francososa <franky.sosa@gmail.com>
2022-05-18 12:30:35 -0600

    Define function to add questions with corresponding route

--- src/main.rs
+++ src/main.rs
@@ -110,6 +110,17 @@ async fn get_questions(
     }
 }
 
+async fn add_question(
+    store: Store, 
+    question: Question,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    store.questions.write().insert(question.id.clone(), question);
+    Ok(warp::reply::with_status(
+        "Question added", 
+        StatusCode::OK,
+    ))
+}
+
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
     if let Some(error) = r.find::<Error>() {
         Ok(warp::reply::with_status(
@@ -139,15 +150,21 @@ async fn main() {
         .allow_header("content-type")
         .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
 
-    let get_items = warp::get()
+    let get_questions = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
         .and(warp::query())
         .and(store_filter.clone())
-        .and_then(get_questions)
-        .recover(return_error);
+        .and_then(get_questions);
 
-    let routes = get_items.with(cors);
+    let add_question = warp::post()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and(store_filter.clone())
+        .and(warp::body::json())
+        .and_then(add_question);
+
+    let routes = get_questions.or(add_question).with(cors).recover(return_error);
 
     warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
